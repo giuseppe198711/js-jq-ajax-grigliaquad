7e2295a6ccac93895b2704466239ed7d3583b196
Giuseppe <giuseppedf8711@gmail.com>
2020-09-16 20:36:37 +0200

    finito aggiungere dettagli

--- script.js
+++ script.js
@@ -5,48 +5,37 @@ $(document).ready (function() {
   // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
   // Il numero ottenuto appare al centro del quadrato.
 
+  var endpoint = "https://flynn.boolean.careers/exercises/api/random/int";
 
 
+  $(".container").click(function() {
+    var elemento = $(this);
+    $.ajax(
 
+     {
+       "url": endpoint,
+       "method": "GET",
+       "success": function (data, stato) {
+         var rispostaServer = data.response;
+         console.log(data.response);
+         elemento.text(rispostaServer);
 
+         if (rispostaServer <= 5) {
+          elemento.addClass("yellow");
+          elemento.removeClass("green");
 
+         } else  {
+          elemento.addClass("green");
+          elemento.removeClass("yellow");
+         }
 
+       },
+       "error": function (richiesta, stato, errori){
+         alert("errore");
+       }
+     }
 
+   );
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+});
